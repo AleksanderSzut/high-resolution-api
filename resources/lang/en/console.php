4d710fbd6d2f25333d18dd39bc5bae6184fca309
Aleksander Szut <aleksander.szut@paymento.pl>
2021-04-01 01:08:19 +0200

    - (fix) change language file format

---
+++ resources/lang/en/console.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    "added" => "added: :count",
+    "updated" => "updated: :count",
+    "rejected" => "rejected: :count",
+    "unchanged" => "unchanged: :count",
+    "unknownError" => "unknown problem when adding characters",
+    "fatalError" => "fatal error :optionalMessage",
+    "header" => "### generating summary ###"
+
+];
