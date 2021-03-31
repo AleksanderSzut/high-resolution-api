b988834147a35023385217fcbb9f8c6a1e499ba7
Aleksander Szut <aleksander.szut@paymento.pl>
2021-03-31 18:23:13 +0200

    - add commend class

---
+++ app/Console/Commands/GetUser.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\CharactersService;
+use App\Services\CharacterApi\CharacterApiAaoiaf;
+use Illuminate\Console\Command;
+
+class GetUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'get-user {count?}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Downloading people from https://anapioficeandfire.com/ and saving them to the database';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $characters = new CharactersService(new CharacterApiAaoiaf, 1);
+    }
+}
